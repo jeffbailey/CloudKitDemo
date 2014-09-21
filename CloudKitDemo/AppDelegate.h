551222e48cf8bc2a991bdbabb283bbc6745c7fce
Jeff Bailey <dogsonic22@gmail.com>
2014-09-21 12:43:05 -0400

    Initial Commit

---
+++ CloudKitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CloudKitDemo
+//
+//  Created by Jeff Bailey on 9/21/14.
+//  Copyright (c) 2014 FourthFrame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CloudKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloudKitDemo
+//
+//  Created by Jeff Bailey on 9/21/14.
+//  Copyright (c) 2014 FourthFrame. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
